058747f5adbe0acf026400693eeaa2f8e931620b
DeengAnggreny <anggreynideeng645@gmail.com>
2022-11-25 20:10:07 +0800

    Tambah search movie

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import Navigation from "./components/Navigation";
 import "./App.css"
+import { useState } from "react";
+
 
 export default function App() {
+  const [popularMovies, setPopularMovies] = useState([]);
+  const search = (query) => {
+    console.log({ query });
+  };
   return (
     <div className="App">
       <Navigation />
       <header className="App-header">
         <h1>Hallo</h1>
+        <input
+          className="Movie-search"
+          placeholder="Cari Film Kesayangan..."
+          onChange={({ target }) => search(target.value)}
+        />
         <div className="Movie-container">
           <div className="Movie-wrapper">
             <div className="Movie-title">Contoh</div>
