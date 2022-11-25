f6976869c785c9b67403ffb92716e74708a6eaf8
DeengAnggreny <anggreynideeng645@gmail.com>
2022-11-25 20:15:14 +0800

    Tambah endpoint api untuk movie dan search

---
+++ src/api/api_movie.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const apiKey = process.env.REACT_APP_APIKEY;
+const baseUrl = process.env.REACT_APP_BASEURL;
+
+export const getMovieList = async () => {
+  const movie = await axios.get(`${baseUrl}/movie/popular?api_key=${apiKey}`);
+  console.log({ movieList: movie });
+  return movie.data.results;
+};
+
+export const searchMovie = async (q) => {
+  const search = await axios.get(q);
+  return;
+};
