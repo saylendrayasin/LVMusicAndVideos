56780101caf0f32e69920efb9e31b57dbd23e69c
GirardRampen18 <rampengirard73@gmail.com>
2022-11-25 19:41:59 +0800

    Menambahkan tampilan untuk tiap kartu

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import Navigation from "./components/Navigation";
+import "./App.css"
 
 export default function App() {
   return (
-    <div className="flex justify-center flex-col">
+    <div className="App">
       <Navigation />
-      <h1 className="text-4xl md:text-3xl font-bold underline bg-red-500">
-        Hello world!
-      </h1>
+      <header className="App-header">
+        <h1>Hallo</h1>
+        <div className="Movie-container">
+          <div className="Movie-wrapper">
+            <div className="Movie-title">Contoh</div>
+            <img className="Movie-image" src="https://placehold.it/100x100" />
+            <div className="Movie-date">20-11-2022</div>
+            <div className="Movie-rate">7.9</div>
+          </div>
+        </div>
+      </header>
+      
     </div>
   );
 }
