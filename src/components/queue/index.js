3a8d8f115d6098ea0bc16b0921a4c5f39a219c84
OvelTjia <s2200046@student.unklab.ac.id>
2022-11-28 03:08:16 +0800

    getting data and styling music player feature

---
+++ src/components/queue/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./queue.css";
+
+export default function Queue() {
+  return (
+    <div>Queue</div>
+  )
+}
--- src/pages/player/index.js
+++ src/pages/player/index.js
@@ -1,5 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import Queue from "../../components/queue";
+import SongCard from "../../components/songCard";
+import apiClient from "../../spotify";
+import "./player.css";
 
 export default function Player() {
-  return <div className="screen-container">player</div>;
+
+  const location = useLocation();
+  const [tracks, setTracks] = useState([]);
+  const [currentTrack, setCurrentTrack] = useState({});
+  const [currentIndex, setCurrentIndex] =useState(0);
+
+  useEffect(() =>{
+    if(location.state){
+      apiClient
+      .get("playlists/" + location.state?.id + "/tracks")
+      .then((res)=> {
+        setTracks(res.data.items);
+        setCurrentTrack(res.data.items[0].track);
+      });
+    }
+  }, [location.state]);
+
+  return (
+    <div className="screen-container flex">
+      <div className="left-player-body">
+
+      </div>
+      <div className="right-player-body">
+        <SongCard album={currentTrack.album}/>
+        <Queue />
+      </div>
+    </div>
+  );
 }
