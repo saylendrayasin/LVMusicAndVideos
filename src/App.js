5a872f52eae31c50fa424eb403ca38e13ed5344d
OvelTjia <s2200046@student.unklab.ac.id>
2022-11-27 22:16:50 +0800

    testing dan styling page musik

--- src/App.js
+++ src/App.js
@@ -4,20 +4,23 @@ import Movies from "./pages/Movies";
 import Details from "./pages/Details";
 import Navigation from "./components/Navigation";
 import MoviesDetails from "./components/MoviesDetail";
+import Home from "./pages/home";
+
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Navigation />
-      <Routes>
-        <Route path="/" element={<Movies />} />
-        {/* <Route path="/About" element={<About />} /> */}
-        <Route path="/details" element={<Details />}>
-          <Route path=":movie_id" element={<MoviesDetails />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
+  return <div><Home /></div>
+  // (
+  //   <BrowserRouter>
+  //     <Navigation />
+  //     <Routes>
+  //       <Route path="/" element={<Movies />} />
+  //       {/* <Route path="/About" element={<About />} /> */}
+  //       <Route path="/details" element={<Details />}>
+  //         <Route path=":movie_id" element={<MoviesDetails />} />
+  //       </Route>
+  //     </Routes>
+  //   </BrowserRouter>
+  // );
 }
 
 export default App;
---
+++ src/pages/library/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+export default function Library() {
+  return <div className="screen-container">Library</div>;
+
+}
