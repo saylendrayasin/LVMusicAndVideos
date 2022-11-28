f1ec33e2f4aa5981369400710fbc333af4af0340
OvelTjia <s2200046@student.unklab.ac.id>
2022-11-29 00:45:17 +0800

    finalisasi penambahan fitur player musik, album, artis, playlist dari api spotify (page terpisah)

--- src/pages/player/index.js
+++ src/pages/player/index.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
+import AudioPlayer from "../../components/audioPlayer";
 import Queue from "../../components/queue";
 import SongCard from "../../components/songCard";
+import Widgets from "../../components/widgets";
 import apiClient from "../../spotify";
 import "./player.css";
 
@@ -18,19 +20,30 @@ export default function Player() {
       .get("playlists/" + location.state?.id + "/tracks")
       .then((res)=> {
         setTracks(res.data.items);
-        setCurrentTrack(res.data.items[0].track);
+        setCurrentTrack(res.data?.items[0]?.track);
       });
     }
   }, [location.state]);
 
+  useEffect(() => {
+    setCurrentTrack(tracks[currentIndex]?.track);
+  }, [currentIndex, tracks]);
+
   return (
     <div className="screen-container flex">
       <div className="left-player-body">
-
+        <AudioPlayer 
+          currentTrack={currentTrack}
+          total={tracks}
+          currentIndex={currentIndex}
+          setCurrentIndex={setCurrentIndex}
+        />
+        <Widgets artistID={currentTrack?.album?.artists[0]?.id} />
       </div>
       <div className="right-player-body">
-        <SongCard album={currentTrack.album}/>
-        <Queue />
+        
+        <SongCard album={currentTrack?.album}/>
+        <Queue tracks={tracks} setCurrentIndex={setCurrentIndex} />
       </div>
     </div>
   );
