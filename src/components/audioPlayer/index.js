b5acc634a43756553c62b7e6b7560f7616f60c62
Saylendra Yasin <saylendrayasin@gmail.com>
2022-11-30 00:55:56 +0800

    Fixing mini bug

--- src/components/audioPlayer/index.js
+++ src/components/audioPlayer/index.js
@@ -4,71 +4,70 @@ import Controls from "./controls";
 import ProgressCircle from "./progressCircle";
 import WaveAnimation from "./waveAnimation";
 
-export default function AudioPlayer({currentTrack, currentIndex, setCurrentIndex, total}) {
-  
+export default function AudioPlayer({
+  currentTrack,
+  currentIndex,
+  setCurrentIndex,
+  total,
+}) {
   const [isPlaying, setIsPlaying] = useState(false);
   const [trackProgress, setTrackProgress] = useState(0);
   var audioSrc = total[currentIndex]?.track.preview_url;
   const audioRef = useRef(new Audio(total[0]?.track.preview_url));
   const intervalRef = useRef();
   const isReady = useRef(false);
-  const {duration} = audioRef.current;
-  const currentPercentage = duration ?(trackProgress/duration)*100:0;
-  const startTimer = () =>{
+  const { duration } = audioRef.current;
+  const currentPercentage = duration ? (trackProgress / duration) * 100 : 0;
+  const startTimer = () => {
     clearInterval(intervalRef.current);
 
-    intervalRef.current = setInterval(() =>{
-      if(audioRef.current.ended){
+    intervalRef.current = setInterval(() => {
+      if (audioRef.current.ended) {
         handleNext();
-      }
-      else{
-        setTrackProgress(audioRef.current.currentTime)
+      } else {
+        setTrackProgress(audioRef.current.currentTime);
       }
     }, [1000]);
   };
 
-  useEffect(() =>{
-    if(audioRef.current.src){
-      if(isPlaying){
+  useEffect(() => {
+    if (audioRef.current.src) {
+      if (isPlaying) {
         audioRef.current.play();
         startTimer();
-      }
-      else{
+      } else {
         clearInterval(intervalRef.current);
         audioRef.current.pause();
       }
-    }
-    else{
-      if(isPlaying){
+    } else {
+      if (isPlaying) {
         audioRef.current = new Audio(audioSrc);
         audioRef.current.play();
         startTimer();
-      }
-      else{
+      } else {
         clearInterval(intervalRef.current);
         audioRef.current.pause();
       }
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isPlaying]);
 
-  useEffect(() =>{
+  useEffect(() => {
     audioRef.current.pause();
     audioRef.current = new Audio(audioSrc);
     setTrackProgress(audioRef.current.currentTime);
-    if(isReady.current){
+    if (isReady.current) {
       audioRef.current.play();
       setIsPlaying(true);
       startTimer();
-    }
-    else{
+    } else {
       isReady.current = true;
     }
-    
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [currentIndex]);
 
   useEffect(() => {
-    
-  
     return () => {
       audioRef.current.pause();
       clearInterval(intervalRef.current);
@@ -76,23 +75,21 @@ export default function AudioPlayer({currentTrack, currentIndex, setCurrentIndex
   }, []);
 
   const handleNext = () => {
-    if(currentIndex < total.length -1){
+    if (currentIndex < total.length - 1) {
       setCurrentIndex(currentIndex + 1);
-    }
-    else{
+    } else {
       setCurrentIndex(0);
     }
   };
 
-  const handlePrev = () =>{
-    if(currentIndex -1 < 0) setCurrentIndex(total.length - 1);
-    else setCurrentIndex(currentIndex -1);
-  }
+  const handlePrev = () => {
+    if (currentIndex - 1 < 0) setCurrentIndex(total.length - 1);
+    else setCurrentIndex(currentIndex - 1);
+  };
 
-  const addZeros = (n) =>{
-    return n > 9? "" + n : "0" + n;
-  }
-  
+  const addZeros = (n) => {
+    return n > 9 ? "" + n : "0" + n;
+  };
 
   const artists = [];
   currentTrack?.album?.artists.forEach((artist) => {
@@ -115,11 +112,11 @@ export default function AudioPlayer({currentTrack, currentIndex, setCurrentIndex
         <p className="song-artist">{artists.join(" | ")}</p>
         <div className="player-right-bottom flex">
           <div className="song-duration flex">
-            <p className="duration">0:{addZeros (Math.round(trackProgress))}</p>
+            <p className="duration">0:{addZeros(Math.round(trackProgress))}</p>
             <WaveAnimation isPlaying={isPlaying} />
             <p className="duration">0:30</p>
           </div>
-          <Controls 
+          <Controls
             isPlaying={isPlaying}
             setIsPlaying={setIsPlaying}
             handleNext={handleNext}
