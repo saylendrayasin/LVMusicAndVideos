6f2ae0a85f45ad38a444b868aa79ab77fc3db35c
Andre Tamboto <andretamboto13@gmail.com>
2022-11-29 17:56:17 +0800

    Tambah UI dan about

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -6,17 +6,20 @@ function AboutUs() {
           <h1 className="font-bold self-center mt-10 mb-20 text-gray-400 text-3xl lg:text-4xl">
             About Us
           </h1>
-          <h2 className="font-bold ml-1.5">This project created by Las Vegas Team Development</h2>
-          <p className="font-bold ml-3">Groups Member</p>
-          <p className="ml-5">1. Yasin, Saylendra Myheart</p>
-          <p className="ml-5">2. Tamboto, Andre</p>
-          <p className="ml-5">3. Tjia, Ovel Richy Maramis </p>
-          <p className="ml-5">4. Dengah, Jofanly Stevanus</p>
-          <p className="ml-5">5. Rampen, Gerard</p>
-          <p className="ml-5">6. Deeng, Anggreiny</p>
+          <h2 className="font-bold self-center">THIS IS OUR PROJECT CREATED BY LAS VEGAS DEVELOPMENT </h2><br></br>
+          <p className="font-bold ml-5">Groups Member :</p>
+          <p className="ml-5">1. Yasin, Saylendra Myheart (INFORMATIKA)</p>
+          <p className="ml-5">2. Tamboto, Andre  (INFORMATIKA)</p>
+          <p className="ml-5">3. Tjia, Ovel Richy Maramis (INFORMATIKA) </p>
+          <p className="ml-5">4. Dengah, Jofanly Stevanus (INFORMATIKA)</p>
+          <p className="ml-5">5. Rampen, Gerard (INFORMATIKA)</p>
+          <p className="ml-5">6. Deeng, Anggreiny (INFORMATIKA)</p><br></br>
 
-          <p className="font-bold self-center mt-40 mb-20 text-gray-400 text-1xl lg:text-1xl">
-            This is the final project from the front-end</p>
+          <p className="ml-5 mt-20 italic">UNIVERSITAS KLABAT</p>
+          <p className="ml-5 italic">COMPUTER SCIENCE</p>
+          <p className="ml-5 italic">FRONT-END DEVELOPMENT</p>
+
+         
         </div>
       );
 }
