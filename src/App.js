8ce76274e8abf5dc38dfc93e26b75c9d8e8b221b
Andre Tamboto <andretamboto13@gmail.com>
2022-11-29 17:02:07 +0800

    Tambah halaman about us

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Movies from "./pages/Movies";
 import Details from "./pages/Details";
 import Navigation from "./components/Navigation";
 import MoviesDetails from "./components/MoviesDetail";
+import AboutUs from "./pages/AboutUs";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Navigation />
       <Routes>
         <Route path="/" element={<Movies />} />
+        <Route path={"/about"} element={<AboutUs />} />
         {/* <Route path="/About" element={<About />} /> */}
         <Route path="/details" element={<Details />}>
           <Route path=":movie_id" element={<MoviesDetails />} />
---
+++ src/pages/AboutUs.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function AboutUs() {
+    return (
+        <div className="flex flex-col w-full min-h-screen text-white bg-black">
+          <h1 className="font-bold self-center mt-10 mb-20 text-gray-400 text-3xl lg:text-4xl">
+            About Us
+          </h1>
+          <h2 className="font-bold ml-1.5">This project created by Las Vegas Team Development</h2>
+          <p className="font-bold ml-3">Groups Member</p>
+          <p className="ml-5">1. Yasin, Saylendra Myheart</p>
+          <p className="ml-5">2. Tamboto, Andre</p>
+          <p className="ml-5">3. Tjia, Ovel Richy Maramis </p>
+          <p className="ml-5">4. Dengah, Jofanly Stevanus</p>
+          <p className="ml-5">5. Rampen, Gerard</p>
+          <p className="ml-5">6. Deeng, Anggreiny</p>
+
+          <p className="font-bold self-center mt-40 mb-20 text-gray-400 text-1xl lg:text-1xl">
+            This is the final project from the front-end</p>
+        </div>
+      );
+}
+
+export default AboutUs
